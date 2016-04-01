b7955a62d2b68bd087ae72175a93e81cd19cf006
pyqf66 <zyf3282@msn.cn>
2016-04-01 15:30:22 +0800

    格式化

--- python/eye_protect.py
+++ python/eye_protect.py
@@ -1,12 +1,15 @@
 # -*- coding:utf-8 -*-
 import os
-from apscheduler.schedulers.blocking import BlockingScheduler 
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+
 CURRENT_DIR = os.getcwd()
 EYE_PROTECT_FILE_DIR = CURRENT_DIR.replace("python", "vbs")
 EYE_PROTECT_FILE_SCRIPT = EYE_PROTECT_FILE_DIR + "\\eye_protect.vbs"
-print(EYE_PROTECT_FILE_SCRIPT)
-schedudler = BlockingScheduler()
-@schedudler.scheduled_job("cron", minute="*/40", hour="*")
+scheduler = BlockingScheduler()
+
+
+@scheduler.scheduled_job("cron", minute="*/40", hour="*")
 def eye_protect():
     os.system(EYE_PROTECT_FILE_SCRIPT)
-schedudler.start()
+scheduler.start()
